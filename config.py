6be946e00d849e17368d9f14d4c1f7f3b63c6a0e
Egor Skidnov <eskidnov@gmail.com>
2018-03-02 00:43:08 +0300

    Поправочка: заменил не тот токен. Продолжаем проверку сбера

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ from  telebot.types import LabeledPrice
 import logging
 
 # TEST
-token = '401643678:TEST:acc764d4-10e3-40c6-9252-d740d0f98574'
-provider_token = '381764678:TEST:4464'
+token = '522784740:AAFE0fNxYt_1NytTIR4913k0VTTq0m1REY'
+provider_token = '401643678:TEST:acc764d4-10e3-40c6-9252-d740d0f98574'
 
 shelve_name = 'shelve.db'
 
